61ab27b028d26a7ae658cb87af717aa488c8ca67
NK <45469562+navapro@users.noreply.github.com>
2020-07-02 15:16:15 -0600

    Display simple harmonic motion using a rectangle.
    Display simple harmonic motion using a rectangle and sin function.

---
+++ sketch.js
@@ -0,0 +1,16 @@
+let angle = 0;
+
+function setup(){
+  createCanvas(400,400);
+}
+
+function draw(){
+  background(0)
+  translate(width/2, height/2);
+  rectMode(CENTER);
+
+  let h = map(sin(angle), -1,1,0,100);
+  fill(255);
+  rect(0,0,10,h);
+  angle+= 0.1;
+}
