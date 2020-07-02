a5acd1d36bb494b972dc50375d0b7a674f66e499
NK <45469562+navapro@users.noreply.github.com>
2020-07-02 16:05:35 -0600

    Created cube wave
    Displays an array of boxes and using sin wave display the box moving like a wave.

--- sketch.js
+++ sketch.js
@@ -1,22 +1,34 @@
 let angle = 0;
-let w = 25;
+let w = 20;
+let ma;
+let maxD;
 
-function setup(){
-  createCanvas(400,400,WEBGL);
+function setup() {
+  createCanvas(400, 400, WEBGL);
+  ma = atan(cos(QUARTER_PI));
+  maxD = dist(0, 0, 200, 200);
 }
 
-function draw(){
-  background(0)
-  
-  rectMode(CENTER);
+function draw() {
+  background(100);
+  ortho(-400, 400, 400, -400, 0, 1000);
+  rotateX(ma);
+  rotateY(-QUARTER_PI);
 
-  let offset = 0;
-  for (let x = 0; x< width; x+=w){
-    let a = angle +offset;
-    let h = map(sin(a), -1,1,0,100);
-    fill(255);
-    rect(x-width/2 +w,0,w-2,h);
-    offset += 0.1;
+  for (let z = 0; z < height; z += w) {
+    for (let x = 0; x < width; x += w) {
+      push();
+      let d = dist(x, z, width / 2, height / 2);
+      let offset = map(d, 0, maxD, -PI, PI);
+      let a = angle + offset;
+      let h = floor(map(sin(a), -1, 1, 100, 300));
+      translate(x - width / 2, 0, z - height / 2);
+      normalMaterial();
+      box(w, h, w);
+      //rect(x - width / 2 + w / 2, 0, w - 2, h);
+      pop();
+    }
   }
-  angle+= 0.1;
+
+  angle -= 0.1;
 }
